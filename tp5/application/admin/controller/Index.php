47278f10e06f9331f8298d4c90d334714b725396
daweicc <www.daweicc@qq.com>
2018-08-15 18:27:20 +0800

    测试

---
+++ tp5/application/admin/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User:daweicc
+ * Date: 2018/8/15
+ * Time: 18:12
+ */
+
+namespace app\admin\controller;
+
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+
+}
